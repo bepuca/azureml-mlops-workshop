b632c0170ddd967859178d153d7a647aa551ba90
bepuca <bpuigcamps@microsoft.com>
2023-08-25 18:26:32 +0200

    Implement dataprep for building dataset splits

---
+++ src/common/core/constants.py
@@ -0,0 +1,13 @@
+class DataFields:
+    FILEPATH = "filepath"
+    LABEL = "label"
+    SET = "set"
+
+
+class DataSets:
+    TRAIN = "train"
+    EVAL = "eval"
+    TEST = "test"
+
+
+DATASET_FILENAME = "dataset_splits.csv"
