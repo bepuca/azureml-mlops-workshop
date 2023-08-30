1eea45edc98bb76ea3448fa7ac9d6e7f46faa269
bepuca <bpuigcamps@microsoft.com>
2023-08-30 12:26:49 +0200

    Remove extras

--- src/random_forest/transform.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from PIL import Image
-
-
-def prepare_sample(image: Image) -> np.array:
-    """Convert `image` to 1D array concatenating rows of pixel intensities"""
-    return np.asarray(image).flatten()
